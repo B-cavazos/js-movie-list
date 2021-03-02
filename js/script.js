e7dc70130d6c8d6a454af69752de6824db374c93
Ludwin Granados <ludwingranados@gmail.com>
2021-03-02 13:25:15 -0800

    added id

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let movies = [
     {
+    id: 1,
     title: 'Gladiator',
     year: '2000',
     genre: 'Historical Drama',
@@ -7,6 +8,7 @@ let movies = [
     description: 'Russel Crowe portrays Roman general Maximus Decimus Meridius, who is betrayed when Commodus, the ambitious son of Emperor Marcus Aurelius, murders his father and seizes the throne. Reduced to slavery, Maximus becomes a gladiator and rises through the ranks of the arena to avenge the murders of his family and his emperor.',
 },
   {
+    id: 2,
     title: 'John Wick',
     year: '2014',
     genre: 'Action, Crime, Thriller',
@@ -14,6 +16,7 @@ let movies = [
     description: 'An ex-hit-man comes out of retirement to track down the gangsters that killed his dog and took everything from him.',
   },
   {
+    id. 3,
     title: 'Akira',
     year: '1988',
     genre: 'Animation, Action, Sci-Fi',
@@ -21,6 +24,7 @@ let movies = [
     description: 'A secret military project endangers Neo-Tokyo when it turns a biker gang member into a rampaging psychic psychopath who can only be stopped by two teenagers and a group of psychics.',
   },
   {
+    id: 4,
     title: 'Midsommar',
     year: '2019',
     genre: 'Drama, Horror, Mystery',
@@ -28,6 +32,7 @@ let movies = [
     description: "A couple travels to Scandinavia to visit a rural hometown's fabled Swedish mid-summer festival. What begins as an idyllic retreat quickly devolves into an increasingly violent and bizarre competition at the hands of a pagan cult.",
   },
   {
+    id: 5,
     title: 'Gtrave of the Fireflies',
     year: '1988',
     genre: 'Animation, Drama, War',
