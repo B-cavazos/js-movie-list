8870cef6cb582e76396c898abffaef1cf4c364bd
Ludwin Granados <ludwingranados@gmail.com>
2021-03-02 13:14:18 -0800

    first co-lab commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let movies = [
+    {
+    title: 'Gladiator',
+    year: '2000',
+    genre: 'Historical Drama',
+    imdb: 8.5,
+    description: 'Russel Crowe portrays Roman general Maximus Decimus Meridius, who is betrayed when Commodus, the ambitious son of Emperor Marcus Aurelius, murders his father and seizes the throne. Reduced to slavery, Maximus becomes a gladiator and rises through the ranks of the arena to avenge the murders of his family and his emperor.',
+},
+  {
+    title: 'John Wick',
+    year: '2014',
+    genre: 'Action, Crime, Thriller',
+    imdb: 7.4,
+    description: 'An ex-hit-man comes out of retirement to track down the gangsters that killed his dog and took everything from him.',
+  },
+  {
+    title: 'Akira',
+    year: '1988',
+    genre: 'Animation, Action, Sci-Fi',
+    imdb: 8.00,
+    description: 'A secret military project endangers Neo-Tokyo when it turns a biker gang member into a rampaging psychic psychopath who can only be stopped by two teenagers and a group of psychics.',
+  },
+  {
+    title: 'Midsommar',
+    year: '2019',
+    genre: 'Drama, Horror, Mystery',
+    imdb: 7.1,
+    description: "A couple travels to Scandinavia to visit a rural hometown's fabled Swedish mid-summer festival. What begins as an idyllic retreat quickly devolves into an increasingly violent and bizarre competition at the hands of a pagan cult.",
+  },
+  {
+    title: 'Gtrave of the Fireflies',
+    year: '1988',
+    genre: 'Animation, Drama, War',
+    imdb: 8.5,
+    description: 'A young boy and his little sister struggle to survive in Japan during World War II.',
+},
+];
+
+
+
